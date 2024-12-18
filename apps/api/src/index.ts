b013770fe24360a3e626e8529f1a8c306ddbfd34
Tiago Azevedo <tiagogadenz@icloud.com>
2024-12-18 07:09:24 -0300

    updateing

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,3 @@
-import { defineAbilityFor } from '@saas/auth'
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
-const ability = defineAbilityFor({ role: 'ADMIN' })
-
-const userCanInvite = ability.can('invite', 'User')
-const userCanDelete = ability.can('delete', 'User')
-
-const useCannoyDeleteOtherUsers = ability.cannot('delete', 'User')
-
-console.log(userCanInvite)
-console.log(userCanDelete)
-console.log(useCannoyDeleteOtherUsers)
+const ability = defineAbilityFor({ role: 'MEMBER', id: '1' })
